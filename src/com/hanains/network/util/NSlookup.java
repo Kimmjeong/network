0471c52de581e86b932ecb80b0da85db58827217
bit-user <bit-user@bit>
2015-11-30 13:00:45 +0900

    NSlookup 과제

--- src/com/hanains/network/util/NSlookup.java
+++ src/com/hanains/network/util/NSlookup.java
@@ -1,5 +1,7 @@
 package com.hanains.network.util;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class NSlookup {
@@ -9,12 +11,25 @@ public class NSlookup {
 		Scanner scanner = new Scanner(System.in);
 
 		while (true) {
-			String hostName = scanner.nextLine();
+			System.out.print("> ");
+			String hostName = scanner.nextLine(); // hostName 입력받기
 			
-			
-			if ("exit".equals(hostName)) {
+			if ("exit".equals(hostName)) { // exit 이 입력되면 종료
 				break;
 			}
+			
+			try {
+			
+				InetAddress[] inetAddress=InetAddress.getAllByName(hostName); // hostName에 해당하는 모든 IP Address 받기
+				
+				for(int i=0;i<inetAddress.length;i++){
+					System.out.println(hostName+" : "+inetAddress[i].getHostAddress());
+				}
+				
+			} catch (UnknownHostException e) { // 알 수 없는 host 입력 시 발생
+				e.printStackTrace();
+			} 
+			
 		}
 
 	}
