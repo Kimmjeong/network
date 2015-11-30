a9f656511f9377b70d310cff408ce8c2622a0d05
bit-user <bit-user@bit>
2015-11-30 10:43:47 +0900

    initial Commit

---
+++ src/com/hanains/network/util/NSlookup.java
@@ -0,0 +1,22 @@
+package com.hanains.network.util;
+
+import java.util.Scanner;
+
+public class NSlookup {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			String hostName = scanner.nextLine();
+			
+			
+			if ("exit".equals(hostName)) {
+				break;
+			}
+		}
+
+	}
+
+}
